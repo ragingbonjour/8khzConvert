a36a8d035dcd9c8168641b54bd1bf02b9faffe79
Tristen Dvernychuk <Tristen.Dvernychuk@interiorhealth.ca>
2022-09-06 11:30:56 -0700

    Added script contents + gitignore for directory contents.
    Uses ffmpeg.exe to convert files in ./input to 8khz mono channels, then
    moves them into the /output folder with "converted" in the filename.

---
+++ 8khzConvert.ps1
@@ -0,0 +1,23 @@
+# Modified from https://www.junian.net/tech/powershell-ffmpeg-batch/    09-06-2022
+
+# Clone this repository into a download location for the fullband audio files, then run it 
+
+# Grabs list of files with wavpack extensions in the directory we're in context of
+$originalVids = Get-ChildItem *.wav -Recurse
+
+foreach ($inputVid in $originalVids) {
+    # Keep the filename, append 'converted' for easy differentiation
+    # May be easiest to pre-name files before applying conversion script
+    $outputClip = [io.path]::ChangeExtension($inputVid.FullName, 'converted.wav')
+
+    # Makes assumption ffmpeg.exe is running locally to the script, but if installed globally then it'll work as well
+    & "$PSScriptRoot/ffmpeg.exe" -loglevel error -hide_banner -i $inputVid.FullName -ar 8000 -ac 1 $outputClip
+        # -ar 8000              = 8khz (G.711 Narrowband)
+        # -ac 1                 = Stereo -> Mono (Single Channel)
+        # -loglevel warning     = Reduce output to minimal possible, with only warnings and worse included
+        # -hide_banner          = Remove usual compilation banner when running 
+        # Source: https://ffmpeg.org/ffmpeg.html#Audio-Options 
+
+    # Whatever file we create, move it into an output folder in context of where we call it
+    Move-Item $outputClip output
+}
